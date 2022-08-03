4b2a856faff22d945a7dca7d4ef93f8ad5214434
Shuvodip Ray <shuvodip@spdev1200.onmicrosoft.com>
2022-08-03 17:22:12 +0530

    removed warning suppression

--- dynamicCommands/gulpfile.js
+++ dynamicCommands/gulpfile.js
@@ -10,9 +10,6 @@ var pkgSolution = require('./config/package-solution.json');
 var webpartVersionIndexes = pkgSolution.solution.version.split('.');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-build.addSuppression(`Warning - lint - src/extensions/dynamicCommandset/DynamicCommandset.ts(33,22): error @microsoft/spfx/no-async-await: Usage of "async" has overhead when using in older browsers.`);
-build.addSuppression(`Warning - lint - src/extensions/customListFormCustomizer/CustomListFormCustomizer.ts(39,22): error @microsoft/spfx/no-async-await: Usage of "async" has overhead when using in older browsers.`);
-build.addSuppression(`Warning - lint - src/extensions/customListFormCustomizer/CustomListFormCustomizer.ts(135,21): error @microsoft/spfx/no-async-await: Usage of "async" has overhead when using in older browsers.`);
 
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
