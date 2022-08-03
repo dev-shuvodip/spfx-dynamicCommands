9900e8d8352ab00eda92de589921a074f5a745aa
Shuvodip Ray <shuvodip@spdev1200.onmicrosoft.com>
2022-08-03 17:21:49 +0530

    disabled annoying eslinting rules

--- dynamicCommands/.eslintrc.js
+++ dynamicCommands/.eslintrc.js
@@ -1,5 +1,20 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
 module.exports = {
   extends: ['@microsoft/eslint-config-spfx/lib/profiles/default'],
-  parserOptions: { tsconfigRootDir: __dirname }
+  parserOptions: { tsconfigRootDir: __dirname },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    "@microsoft/spfx/no-async-await": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/naming-convention": "off",
+    "@typescript-eslint/member-ordering": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-unused-vars": "off"
+  }
 };
